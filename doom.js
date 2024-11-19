d3d58b9aa98ec3846c6d99d872c6f2e10eedd70b
serikerkanat <96571375+serikerkanat@users.noreply.github.com>
2024-11-19 11:33:57 +0500

    Update doom.js

--- doom.js
+++ doom.js
@@ -39,15 +39,7 @@ const channelId = 'UC-COWIm1ZSTTi7zYz71kfsw';
 fetch(`https://www.googleapis.com/youtube/v3/search?key=${youtubeApiKey}&channelId=${channelId}&order=date&part=snippet&type=video`)
     .then(response => response.json())
     .then(data => {
-        console.log('API Response:', data); // Проверяем ответ от API
         const videos = data.items;
-
-        // Проверяем, есть ли видео
-        if (!videos || videos.length === 0) {
-            console.error('No videos found or data.items is undefined');
-            return;
-        }
-
         const videoList = document.getElementById('video-list');
         videos.forEach(video => {
             const videoElement = document.createElement('div');
